f38d5a1b4801565cf23e0d0066a1081d68d1cfa6
Sumit Thakur <samtak190@gmail.com>
2025-11-26 11:39:59 +0530

    SetUp

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './pages/Home'
+
+const App = () => {
+  return <div className='text-5xl'>
+    <Home />
+  </div>;
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    return <div className='container'>
+        <Navbar />
+    </div>;
+}
+
+
+export default Home;
